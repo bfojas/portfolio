28da8f2c3ec15163c965529f02db938302d48561
Bradley Fojas <brad.fojas@yahoo.com>
2019-02-20 08:50:32 -0700

    Updates

---
+++ precache-manifest.5cea48a62ceb28098dc51ea6e3ae05e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9203bb164935ce32c89fc6e8ed1334b4",
+    "url": "/static/media/welcome_large.9203bb16.jpg"
+  },
+  {
+    "revision": "da7789ee5eaa1fea7ed301e53ba8fe9c",
+    "url": "/static/media/dog_profile.da7789ee.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b407edaf06d05b338a29",
+    "url": "/static/js/main.b407edaf.chunk.js"
+  },
+  {
+    "revision": "8c50c144ea946196a05f",
+    "url": "/static/js/1.8c50c144.chunk.js"
+  },
+  {
+    "revision": "b407edaf06d05b338a29",
+    "url": "/static/css/main.63298307.chunk.css"
+  },
+  {
+    "revision": "7b24c93fecf125f504938d582c86e1f2",
+    "url": "/index.html"
+  }
+];
