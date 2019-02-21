822200fdfb471276a3e51e56a466e2a6670f9fb5
Bradley Fojas <brad.fojas@yahoo.com>
2019-02-20 19:24:43 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.a3104cae09197b59c8c63050a8b353e6.js"
+  "/portfolio/precache-manifest.bf097b7ef0303472ec19131e01556d4f.js"
 );
 
 workbox.clientsClaim();
