8db59cdcab9523ac473898064de32bbb9d20a9c9
Bradley Fojas <brad.fojas@yahoo.com>
2019-02-20 18:16:57 -0700

    image files changed

--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -28,7 +28,7 @@ export function renderRoadTrip () {
         type: ROAD_TRIP,
         payload: {
             name: "THE ROAD TRIP",
-            image: "../images/road_trip.png",
+            image: "../images/road_trip.jpg",
             about: "A collaborative group project to bring the power of the Google Maps API to a user-friendly road trip planning app. Users can edit routes with a quick drag and drop, explore other trips and other road trippers.",
             techUsed: "ReactJs  |  Express/NodeJs  |  JavaScript  |  HTML  |  CSS  |  Redux  |  PostgreSQL  |  SASS  | Amazon AWS  |  B-Crypt  |  React-Router"
         }
@@ -40,7 +40,7 @@ export function renderCardFun () {
         type: CARD_FUN,
         payload: {
             name: "THE CARD GAME",
-            image: "../images/card_fun.png",
+            image: "../images/card_fun.jpg",
             about: "It's a simple card game, but baby got back-end. Users play and chat together over web sockets in this card guessing game. Players have many profile customizations and a bank system to make the fun feel riskier.",
             techUsed: "ReactJs  |  Express/NodeJs  |  JavaScript  |  HTML  |  CSS  |  Redux  |  Web Sockets  |  PostgreSQL  |  Nginx  |  Digital Ocean  |  SASS  | Amazon AWS  |  Stripe  |   Auth0  |  ChartJs  |  React-Router"
         }
@@ -52,7 +52,7 @@ export function renderBlackjack () {
         type: BLACKJACK,
         payload: {
             name: "THE BLACKJACK",
-            image: "../images/blackjack.png",
+            image: "../images/blackjack.jpg",
             about: "A standard blackjack app that I put together after only coding for a few weeks. This app lets the player play hands of blackjack against a dealer. Aces and busting logic are all there.",
             techUsed: "ReactJs  |  Express/NodeJs  |  JavaScript  |  HTML  |  CSS"
         }
