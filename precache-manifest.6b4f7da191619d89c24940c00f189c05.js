c31cc9fea706f9716e5bf5b4a0ed98bfc05d93be
Bradley Fojas <brad.fojas@yahoo.com>
2019-06-13 16:10:51 -0700

    Updates

---
+++ precache-manifest.6b4f7da191619d89c24940c00f189c05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d4054f0277b957abe3d71e598679477",
+    "url": "/portfolio/static/media/cartoon.0d4054f0.png"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "87bf0cdb7cfedf793890",
+    "url": "/portfolio/static/js/main.87bf0cdb.chunk.js"
+  },
+  {
+    "revision": "8ac93153f71b286cf3cc",
+    "url": "/portfolio/static/js/1.8ac93153.chunk.js"
+  },
+  {
+    "revision": "87bf0cdb7cfedf793890",
+    "url": "/portfolio/static/css/main.1a8f1683.chunk.css"
+  },
+  {
+    "revision": "dc34c9eb916ab5180ce119f4d94d0817",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.3c9211cbdb5a5d118f2ec0e325315d5d.js"
+  "/portfolio/precache-manifest.6b4f7da191619d89c24940c00f189c05.js"
 );
 
 workbox.clientsClaim();
