07d53a3a7ca3b1bfd3888bee2f82d3a0e51450f6
Bradley Fojas <43628838+bfojas@users.noreply.github.com>
2020-07-17 10:50:50 -0700

    Updates

---
+++ precache-manifest.899c264aec36de9e460a1cf966596d7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fdc4d51b5e7f96fe135fda783dfac97",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8ab8018f8b36c917c7ab",
+    "url": "/portfolio/static/css/main.0a02e8db.chunk.css"
+  },
+  {
+    "revision": "93745d6e9a8a18b87469",
+    "url": "/portfolio/static/js/2.97cfc4ff.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.97cfc4ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ab8018f8b36c917c7ab",
+    "url": "/portfolio/static/js/main.e6097c86.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "0d4054f0277b957abe3d71e598679477",
+    "url": "/portfolio/static/media/cartoon.0d4054f0.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.e7e8c3f256c12f1433f6d3b84ea34ee6.js"
+  "/portfolio/precache-manifest.899c264aec36de9e460a1cf966596d7d.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/portfolio/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
